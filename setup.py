597fa93b4db78dc3262d3f9d9e6fa87d07ce9d09
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-01-20 11:50:04 +0100

    Use rich for console output

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         "Click",
         "libvirt-python",
-        "tabulate",
+        "rich",
     ],
     entry_points={
         "console_scripts": [
--- vms.py
+++ vms.py
@@ -7,20 +7,22 @@ import json
 import libvirt
 import re
 import sys
-import tabulate
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
 from xml.etree import ElementTree
 
 
 SOURCE_ATTRIBUTES = {"file", "dir", "name", "dev", "volume"}
 STATES = {
-    libvirt.VIR_DOMAIN_RUNNING: "running",
-    libvirt.VIR_DOMAIN_SHUTDOWN: "shutting down",
-    libvirt.VIR_DOMAIN_SHUTOFF: "stopped",
-    libvirt.VIR_DOMAIN_PAUSED: "paused",
-    libvirt.VIR_DOMAIN_NOSTATE: "unknown",
-    libvirt.VIR_DOMAIN_BLOCKED: "blocked",
-    libvirt.VIR_DOMAIN_CRASHED: "crashed",
-    libvirt.VIR_DOMAIN_PMSUSPENDED: "suspended",
+    libvirt.VIR_DOMAIN_RUNNING: {"label": "running", "style": "green"},
+    libvirt.VIR_DOMAIN_SHUTDOWN: {"label": "shutting down"},
+    libvirt.VIR_DOMAIN_SHUTOFF: {"label": "stopped"},
+    libvirt.VIR_DOMAIN_PAUSED: {"label": "paused", "style": "blue"},
+    libvirt.VIR_DOMAIN_NOSTATE: {"label": "unknown", "style": "red"},
+    libvirt.VIR_DOMAIN_BLOCKED: {"label": "blocked", "style": "red"},
+    libvirt.VIR_DOMAIN_CRASHED: {"label": "crashed", "style": "red"},
+    libvirt.VIR_DOMAIN_PMSUSPENDED: {"label": "suspended", "style": "blue"},
 }
 
 
@@ -31,7 +33,11 @@ def complete_domain_pattern(ctx, param, incomplete):
     domains = []
     connect = ctx.find_root().params["connect"]
     with connect_libvirt(connect) as cnx:
-        domains = [dom.name() for dom in cnx.listAllDomains() if dom.name().startswith(incomplete)]
+        domains = [
+            dom.name()
+            for dom in cnx.listAllDomains()
+            if dom.name().startswith(incomplete)
+        ]
     return domains
 
 
@@ -61,16 +67,19 @@ def list(ctx, format, patterns):
         for dom in ctx.obj.listAllDomains()
         if matches(dom.name(), patterns)
     ]
+    console = Console()
     if format == "json":
-        print(json.dumps(domains))
+        domains = [[domain[0], domain[1]["label"]] for domain in domains]
+        console.print(json.dumps(domains))
     else:
-        print(
-            tabulate.tabulate(
-                sorted(domains, key=lambda d: d[0]),
-                headers=["Name", "State"],
-                tablefmt="simple",
-            )
-        )
+        table = Table(show_header=True)
+        table.add_column("Name")
+        table.add_column("State")
+        for name, state in sorted(domains, key=lambda d: d[0]):
+            formatted_state = Text()
+            formatted_state.append(state["label"], style=state.get("style"))
+            table.add_row(name, formatted_state)
+        console.print(table)
 
 
 @cli.command()
@@ -82,18 +91,20 @@ def start(ctx, patterns):
 
     PATTERNS: the list of patterns matching the VM name. If none is set matches all VMs.
     """
-    for dom in ctx.obj.listAllDomains():
-        if dom.state()[0] != libvirt.VIR_DOMAIN_RUNNING and matches(
-            dom.name(), patterns
-        ):
-            try:
-                print("Starting " + dom.name())
-                dom.create()
-            except libvirt.libvirtError as err:
-                print(
-                    "Failed to start {}: {}".format(dom.name(), err),
-                    file=sys.stderr,
-                )
+    console = Console()
+    with console.status("[bold green]Starting domains..."):
+        for dom in ctx.obj.listAllDomains():
+            if dom.state()[0] != libvirt.VIR_DOMAIN_RUNNING and matches(
+                dom.name(), patterns
+            ):
+                try:
+                    dom.create()
+                    console.print(dom.name() + " started")
+                except libvirt.libvirtError as err:
+                    console.print(
+                        "Failed to start {}: {}".format(dom.name(), err),
+                        style="bold red",
+                    )
 
 
 @cli.command(help="")
@@ -105,18 +116,20 @@ def stop(ctx, patterns):
 
     PATTERNS: the list of patterns matching the VM name. If none is set matches all VMs.
     """
-    for dom in ctx.obj.listAllDomains():
-        if dom.state()[0] == libvirt.VIR_DOMAIN_RUNNING and matches(
-            dom.name(), patterns
-        ):
-            try:
-                print("Stopping " + dom.name())
-                dom.shutdown()
-            except libvirt.libvirtError as err:
-                print(
-                    "Failed to shut down {}: {}".format(dom.name(), err),
-                    file=sys.stderr,
-                )
+    console = Console()
+    with console.status("[bold green]Stopping domains..."):
+        for dom in ctx.obj.listAllDomains():
+            if dom.state()[0] == libvirt.VIR_DOMAIN_RUNNING and matches(
+                dom.name(), patterns
+            ):
+                try:
+                    dom.shutdown()
+                    console.print("Triggered shutdown of " + dom.name())
+                except libvirt.libvirtError as err:
+                    console.print(
+                        "Failed to shut down {}: {}".format(dom.name(), err),
+                        style="bold red",
+                    )
 
 
 @cli.command()
@@ -129,94 +142,100 @@ def delete(ctx, patterns):
     PATTERNS: the list of patterns matching the VM name. If none is set matches all VMs.
     """
     domains = [dom for dom in ctx.obj.listAllDomains() if matches(dom.name(), patterns)]
-    for dom in domains:
-        name = dom.name()
-        if dom.state()[0] == libvirt.VIR_DOMAIN_RUNNING:
-            print("Stopping " + name)
-            dom.destroy()
+    console = Console()
+    with console.status("[bold green]Deleting domains..."):
+        for dom in domains:
+            name = dom.name()
+            if dom.state()[0] == libvirt.VIR_DOMAIN_RUNNING:
+                dom.destroy()
+                console.print("Stopped " + name)
 
-        volumes_to_delete = []
-        xml_desc = ElementTree.fromstring(dom.XMLDesc())
-        for disk in xml_desc.findall("./devices/disk"):
-            target_node = disk.find("target")
-            if target_node is None:
-                print(
-                    "Missing target in disk definition of vm " + name,
-                    file=sys.stderr,
+            volumes_to_delete = []
+            xml_desc = ElementTree.fromstring(dom.XMLDesc())
+            for disk in xml_desc.findall("./devices/disk"):
+                target_node = disk.find("target")
+                if target_node is None:
+                    console.print(
+                        "Missing target in disk definition of vm " + name,
+                        style="bold red",
+                    )
+                    continue
+                target = target_node.get("dev")
+
+                source_node = disk.find("source")
+                if source_node is not None:
+                    attr = SOURCE_ATTRIBUTES.intersection(set(source_node.keys()))
+                    if len(attr) != 1:
+                        continue
+                    source = source_node.get(attr.pop())
+                    pool = source_node.get("pool")
+                    volume = None
+                    if pool is not None:
+                        try:
+                            pool_obj = ctx.obj.storagePoolLookupByName(pool)
+                        except libvirt.libvirtError:
+                            console.print(
+                                "Storage pool {} not found for disk {} of vm {}".format(
+                                    pool, target, name
+                                ),
+                                style="bold red",
+                            )
+                            continue
+
+                        try:
+                            volume = pool_obj.storageVolLookupByName(source)
+                        except libvirt.libvirtError:
+                            console.print(
+                                "Storage volume {}/{} not found for disk {} of vm {}".format(
+                                    pool, source, target, name
+                                ),
+                                style="bold red",
+                            )
+                            continue
+
+                    else:
+                        try:
+                            volume = ctx.obj.storageVolLookupByPath(source)
+                        except libvirt.libvirtError:
+                            console.print(
+                                "Storage volume {} of vm {} not managed by libvirt, delete manually".format(
+                                    source, name
+                                ),
+                                style="bold red",
+                            )
+                            continue
+
+                    if volume:
+                        volumes_to_delete.append(
+                            {"volume": volume, "target": target, "source": source}
+                        )
+
+            try:
+                dom.undefineFlags(
+                    libvirt.VIR_DOMAIN_UNDEFINE_MANAGED_SAVE
+                    | libvirt.VIR_DOMAIN_UNDEFINE_SNAPSHOTS_METADATA
+                    | libvirt.VIR_DOMAIN_UNDEFINE_CHECKPOINTS_METADATA
+                    | libvirt.VIR_DOMAIN_UNDEFINE_NVRAM
+                )
+                console.print("Deleted " + name)
+            except libvirt.libvirtError as err:
+                console.print(
+                    "Failed to delete {}: {}".format(name, err), style="bold red"
                 )
                 continue
-            target = target_node.get("dev")
 
-            source_node = disk.find("source")
-            if source_node is not None:
-                attr = SOURCE_ATTRIBUTES.intersection(set(source_node.keys()))
-                if len(attr) != 1:
-                    continue
-                source = source_node.get(attr.pop())
-                pool = source_node.get("pool")
-                volume = None
-                if pool is not None:
-                    try:
-                        pool_obj = ctx.obj.storagePoolLookupByName(pool)
-                    except libvirt.libvirtError:
-                        print(
-                            "Storage pool {} not found for disk {} of vm {}".format(
-                                pool, target, name
-                            ),
-                            file=sys.stderr,
-                        )
-                        continue
-
-                    try:
-                        volume = pool_obj.storageVolLookupByName(source)
-                    except libvirt.libvirtError:
-                        print(
-                            "Storage volume {}/{} not found for disk {} of vm {}".format(
-                                pool, source, target, name
-                            ),
-                            file=sys.stderr,
-                        )
-                        continue
-
-                else:
-                    try:
-                        volume = ctx.obj.storageVolLookupByPath(source)
-                    except libvirt.libvirtError:
-                        print(
-                            "Storage volume {} of vm {} not managed by libvirt, delete manually".format(
-                                source, name
-                            ),
-                            file=sys.stderr,
-                        )
-                        continue
-
-                if volume:
-                    volumes_to_delete.append(
-                        {"volume": volume, "target": target, "source": source}
+            for volume in volumes_to_delete:
+                try:
+                    vol_source = volume["source"]
+                    volume["volume"].delete()
+                    console.print("Volume {} deleted".format(vol_source), style="dim")
+                except libvirt.libvirtError as err:
+                    console.print(
+                        "Failed to delete volume {}({}): {}".format(
+                            volume["target"], volume["source"], err
+                        ),
+                        style="bold red",
                     )
-
-        try:
-            print("Deleting " + name)
-            dom.undefineFlags(
-                libvirt.VIR_DOMAIN_UNDEFINE_MANAGED_SAVE
-                | libvirt.VIR_DOMAIN_UNDEFINE_SNAPSHOTS_METADATA
-                | libvirt.VIR_DOMAIN_UNDEFINE_CHECKPOINTS_METADATA
-                | libvirt.VIR_DOMAIN_UNDEFINE_NVRAM
-            )
-        except libvirt.libvirtError as err:
-            print("Failed to delete {}: {}".format(name, err), file=sys.stderr)
-            continue
-
-        for volume in volumes_to_delete:
-            try:
-                volume["volume"].delete()
-            except libvirt.libvirtError as err:
-                print(
-                    "Failed to delete volume {}({}): {}".format(
-                        volume["target"], volume["source"], err
-                    ),
-                    file=sys.stderr,
-                )
 
 
 @cli.group(help="Snapshots management", invoke_without_command=True)
@@ -263,23 +282,23 @@ def snapshot_list(ctx, format, patterns):
                 ]
             )
 
+    console = Console()
     if format == "json":
-        print(json.dumps(snapshots))
+        console.print(json.dumps(snapshots))
     else:
-        print(
-            tabulate.tabulate(
-                snapshots,
-                headers=[
-                    "Domain",
-                    "Name",
-                    "Current",
-                    "State",
-                    "Created",
-                    "Description",
-                ],
-                tablefmt="simple",
-            )
-        )
+        table = Table(show_header=True)
+        for header in [
+            "Domain",
+            "Name",
+            "Current",
+            "State",
+            "Created",
+            "Description",
+        ]:
+            table.add_column(header)
+        for snapshot in snapshots:
+            table.add_row(*snapshot)
+        console.print(table)
 
 
 @snapshot.command(name="create")
@@ -296,14 +315,19 @@ def snapshot_create(ctx, name, patterns):
     """
     name_node = "<name>{}</name>".format(name) if name else ""
     snapshotXml = "<domainsnapshot>{}</domainsnapshot>".format(name_node)
-    for dom in ctx.obj.listAllDomains():
-        if not matches(dom.name(), patterns):
-            continue
-        try:
-            print("Creating snapshot for " + dom.name())
-            dom.snapshotCreateXML(snapshotXml)
-        except libvirt.libvirtError as err:
-            print("Failed to create snapshot on {}: {}".format(dom.name(), err))
+    console = Console()
+    with console.status("[bold green]Creating snapshots..."):
+        for dom in ctx.obj.listAllDomains():
+            if not matches(dom.name(), patterns):
+                continue
+            try:
+                dom.snapshotCreateXML(snapshotXml)
+                console.print("Created snapshot for " + dom.name())
+            except libvirt.libvirtError as err:
+                console.print(
+                    "Failed to create snapshot on {}: {}".format(dom.name(), err),
+                    style="bold red",
+                )
 
 
 @snapshot.command(name="delete")
@@ -318,18 +342,27 @@ def snapshot_delete(ctx, name, patterns):
 
     PATTERNS: the list of patterns matching the VM name. If none is set matches all VMs.
     """
-    for dom in ctx.obj.listAllDomains():
-        if not matches(dom.name(), patterns):
-            continue
-        try:
-            snapshots = [snap for snap in dom.listAllSnapshots() if snap.getName() == name]
-            if len(snapshots) == 1:
-                print("Deleting snapshot for " + dom.name())
-                snapshots[0].delete()
-            else:
-                print("No snapshot to delete for " + dom.name())
-        except libvirt.libvirtError as err:
-            print("Failed to delete snapshot on {}: {}".format(dom.name(), err))
+    console = Console()
+    with console.status("[bold green]Deleting snapshots..."):
+        for dom in ctx.obj.listAllDomains():
+            if not matches(dom.name(), patterns):
+                continue
+            try:
+                snapshots = [
+                    snap for snap in dom.listAllSnapshots() if snap.getName() == name
+                ]
+                if len(snapshots) == 1:
+                    snapshots[0].delete()
+                    console.print("Deleted snapshot for " + dom.name())
+                else:
+                    console.print(
+                        "No snapshot to delete for " + dom.name(), style="orange"
+                    )
+            except libvirt.libvirtError as err:
+                console.print(
+                    "Failed to delete snapshot on {}: {}".format(dom.name(), err),
+                    style="bold red",
+                )
 
 
 @snapshot.command(name="revert")
@@ -344,18 +377,29 @@ def snapshot_revert(ctx, name, patterns):
 
     PATTERNS: the list of patterns matching the VM name. If none is set matches all VMs.
     """
-    for dom in ctx.obj.listAllDomains():
-        if not matches(dom.name(), patterns):
-            continue
-        try:
-            snapshots = [snap for snap in dom.listAllSnapshots() if snap.getName() == name]
-            if len(snapshots) == 1:
-                print("Reverting to snapshot for " + dom.name())
-                dom.revertToSnapshot(snapshots[0])
-            else:
-                print("No snapshot to revert to for " + dom.name())
-        except libvirt.libvirtError as err:
-            print("Failed to revert to snapshot {} on {}: {}".format(name, dom.name(), err))
+    console = Console()
+    with console.status("[bold green]Reverting snapshots..."):
+        for dom in ctx.obj.listAllDomains():
+            if not matches(dom.name(), patterns):
+                continue
+            try:
+                snapshots = [
+                    snap for snap in dom.listAllSnapshots() if snap.getName() == name
+                ]
+                if len(snapshots) == 1:
+                    dom.revertToSnapshot(snapshots[0])
+                    console.print("Reverted to snapshot for " + dom.name())
+                else:
+                    console.print(
+                        "No snapshot to revert to for " + dom.name(), style="orange"
+                    )
+            except libvirt.libvirtError as err:
+                console.print(
+                    "Failed to revert to snapshot {} on {}: {}".format(
+                        name, dom.name(), err
+                    ),
+                    style="bold red",
+                )
 
 
 def matches(name, patterns):
